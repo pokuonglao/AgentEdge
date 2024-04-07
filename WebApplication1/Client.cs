87b98add0093d1200d1e66d08c1bf76e152160a2
pokuong lao <pokuonglao123@gmail.com>
2024-04-07 10:28:15 -0700

    Add project files.

---
+++ WebApplication1/Client.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1
+{
+    public class Clients
+    {
+        public int ID { get; set; }  // Add the ID property
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+
+}
---
+++ WebApplication1/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Clients> clients { get; set; } // DbSet representing the Clients table
+    }
+}
