9ef472aa718ba569d76bb8163e59b16d142428f2
pokuong lao <pokuonglao123@gmail.com>
2024-04-17 14:49:46 -0700

    1

--- reactproject2/vite.config.js
+++ reactproject2/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        // Custom MIME types for specific file extensions
+        mimeTypes: {
+            '.js': 'text/jsx', // Set MIME type for .js files to text/jsx
+        },
+    },
 })
