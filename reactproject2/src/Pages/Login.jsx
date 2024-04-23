8d640f439845eef7d780311ebd5085d5878d6c45
pokuong lao <pokuonglao123@gmail.com>
2024-04-23 16:08:31 -0700

    1

--- reactproject2/src/Pages/Login.jsx
+++ reactproject2/src/Pages/Login.jsx
@@ -24,7 +24,7 @@ const LoginForm = ({ setIsAuthenticated }) => {
     };
 
     const authenticate = (username, password) => {
-        fetch('http://ec2-54-183-197-75.us-west-1.compute.amazonaws.com/Accounts/login', {
+        fetch('https://ec2-54-183-197-75.us-west-1.compute.amazonaws.com/Accounts/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
