fe123ea038cbd9cb8c7c9f17bc69311e7da82cdf
pokuong lao <pokuonglao123@gmail.com>
2024-04-23 21:42:53 -0700

    1

--- reactproject2/src/Components/LoadingSpinner.jsx
+++ reactproject2/src/Components/LoadingSpinner.jsx
@@ -3,10 +3,7 @@ import '../Styles/Components/LoadingSpinner.css';
 
 function LoadingSpinner() {
     return (
-        <div className="loading-spinner">
-            {/* Use any loading spinner or animation library, or create your own */}
-            <div className="spinner"></div>
-        </div>
+        <div className="loading-spinner"></div>
     );
 }
 
