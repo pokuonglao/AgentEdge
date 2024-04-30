9c9398cee3113e85d91e0327a16b7e55228cabcc
pokuong lao <pokuonglao123@gmail.com>
2024-04-30 12:35:44 -0700

    Reapply "11"
    This reverts commit f2fa703b3c2bfca5e088a4991ac182dbdfe7ccd6.

---
+++ WebApplication1/DbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Entities;
+
+namespace WebApplication1
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Clients> clients { get; set; } // DbSet representing the Clients table
+        public DbSet<Accounts> accounts { get; set; } 
+    }
+}
---
+++ WebApplication1/Entities/Accounts.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Entities
+{
+    public class Accounts
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
